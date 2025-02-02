2ecd906f32e36d99d4696cef586699ebec1fb4ab
Nicolas <nicolassanmar@gmail.com>
2025-02-02 02:58:57 -0300

    faster itau download

--- scripts/itau-bookmarklet.js
+++ scripts/itau-bookmarklet.js
@@ -14,15 +14,44 @@ void (async () => {
   // get all options
   const options = selectHistory?.getElementsByTagName("option");
 
-  // iterate over options
+  const optionsValues = Array.from(options).map((option) => option.value);
+
+  const excelDownloadLink = window.document.getElementById("a_excel").href;
+  console.log("🚀 ~ void ~ excelDownloadLink:", excelDownloadLink);
+
+  const excelEndpoint = excelDownloadLink.split("?")[0];
+
+  await Promise.all(
+    optionsValues.map(async (optionValue) => {
+      const month = optionValue.split("-")[0];
+      const year = optionValue.split("-")[1];
+
+      const url = `${excelEndpoint}?anio=${year}&mes=${month}`;
+
+      // download excel in browser
+      const response = await fetch(url);
+      const blob = await response.blob();
+      const urlBlob = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = urlBlob;
+      a.download = `itau-${year}-${month}.xls`;
+      a.click();
+    }),
+  );
 
   for (let i = 0; i < options?.length; i++) {
     const option = options[i];
-    option.selected = true;
 
+    selectHistory.click();
+    await sleep(200);
+    // Click the option element to select it
+    option.click();
+
+    // Dispatch a change event to ensure any listeners are triggered
+    selectHistory.dispatchEvent(new Event("change", { bubbles: true }));
     await sleep(1000);
 
-    // download excel
+    // download excel after the option change effects have been applied
     window.document.getElementById("a_excel")?.click();
   }
 })();
